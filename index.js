9fff5f7b0971e60af0e08d54157e19d202de3fba
Robert Durbin <robert.durbin@gmail.com>
2019-02-09 21:50:21 -0500

    added route for callback authentication

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
-
 const app = express();
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
@@ -16,5 +16,8 @@ app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
+app.get('auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
